2e3d870edd6e0d38fea48d97dc36ed728f52f645
Seweryn Bidolach <s.bidolach@microstep.pl>
2018-12-15 09:16:30 +0000

    Add search service interface to fetch proper data from address book

---
+++ src/main/java/com/gumtree/book/service/SearchService.java
@@ -0,0 +1,32 @@
+package com.gumtree.book.service;
+
+import com.gumtree.book.model.AddressBook;
+import com.gumtree.book.model.Gender;
+
+public interface SearchService {
+
+    /**
+     * This method return number of selected type of gender
+     *
+     * @param gender
+     * @return number of genders
+     */
+    int countGenderByType(final Gender gender);
+
+    /**
+     * This method return the oldest person from the address book
+     *
+     * @return the oldest person
+     */
+    AddressBook findTheOldestPerson();
+
+    /**
+     * This method calculate days between two dates of birth from selected people
+     *
+     * @param fullName1
+     * @param fullName2
+     * @return number of days
+     */
+    long calculateDaysBetweenTwoPeopleDateOfBirth(final String fullName1, final String fullName2);
+
+}
