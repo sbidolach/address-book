bf060cb718628a1872b3f2d42bc7bff802d6a2d2
Seweryn Bidolach <s.bidolach@microstep.pl>
2018-12-15 09:35:51 +0000

    Update unit tests of search service and add mock data

--- src/test/java/com/gumtree/book/service/SearchServiceImplTest.java
+++ src/test/java/com/gumtree/book/service/SearchServiceImplTest.java
@@ -1,19 +1,34 @@
 package com.gumtree.book.service;
 
+import com.gumtree.book.file.FileParser;
 import com.gumtree.book.model.AddressBook;
 import com.gumtree.book.model.Gender;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class SearchServiceImplTest {
 
     private SearchService searchService;
+    private FileParser fileParser;
 
     @Before
     public void setUp() throws Exception {
-        this.searchService = new SearchServiceImpl();
+        fileParser = mock(FileParser.class);
+        searchService = new SearchServiceImpl(fileParser);
+        when(fileParser.readDataFromFile()).thenReturn(new ArrayList<AddressBook>(){{
+            add(new AddressBook("Bill McKnight", Gender.MALE, new Date()));
+            add(new AddressBook("Paul Robinson", Gender.MALE, new Date()));
+            add(new AddressBook("Gemma Lane", Gender.FEMALE, new Date()));
+            add(new AddressBook("Sarah Stone", Gender.FEMALE, new Date()));
+            add(new AddressBook("Wes Jackson", Gender.MALE, new Date()));
+        }});
     }
 
     @Test
@@ -25,9 +40,9 @@ public class SearchServiceImplTest {
     public void findTheOldestPersonOK() {
         AddressBook theOldestPerson = searchService.findTheOldestPerson();
         assertNotNull(theOldestPerson);
-        assertEquals("", theOldestPerson.getFullName());
-        assertEquals("", theOldestPerson.getGender());
-        assertEquals("", theOldestPerson.getGender());
+        assertEquals("Wes Jackson", theOldestPerson.getFullName());
+        assertEquals(Gender.MALE, theOldestPerson.getGender());
+        assertEquals(new Date(), theOldestPerson.getDateOfBirth());
     }
 
     @Test
